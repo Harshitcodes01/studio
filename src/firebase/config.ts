98505a54cbef17deeceb1d582f7c5a5ac4450264
Sports Craze <crazesports537@gmail.com>
2026-01-10 17:54:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8667412964-57e4e",
+  "appId": "1:655785079312:web:77425fef46e6a146231095",
+  "apiKey": "AIzaSyDydLD2qeqzm92EJqSZcYf-T_P_TLeboaw",
+  "authDomain": "studio-8667412964-57e4e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "655785079312"
+};
